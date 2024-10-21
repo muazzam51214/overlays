6549e5dcb02663b8daf50cf70305bcce664b32c2
muazzam51214 <syedmuazzamabbas2004@gmail.com>
2024-10-21 20:27:29 +0500

    User Authentication Completed

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -117,4 +117,147 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User Logged Out Successfully"));
 });
 
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized Request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid Refresh Token");
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh Token is expired or used");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access Token Refreshed Successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(400, error?.message || "Invalid refresh token");
+  }
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid Old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password Changed Successfully"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current User Fetched Successfully"));
+});
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if ([fullName, email].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All field are required!");
+  }
+
+  const existedUser = await User.findOne({ email });
+  if (existedUser) {
+    throw new ApiError(
+      409,
+      "User with email already exists so you can't change your email!"
+    );
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account Details Updated Successfully"));
+});
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({}).select("-password -refreshToken");
+  if (!users) {
+    throw new ApiError(400, "No User Found!");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, users, "All Users Fetched Successfully"));
+});
+
+const getUserById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id).select("-password");
+  if (!user) {
+    throw new ApiError(400, "No User Found!");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "User Fetched By ID Successfully"));
+});
+
+const deleteUserById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findByIdAndDelete(id);
+  if (!user) {
+    throw new ApiError(400, "No User Found!");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "User Deleted Successfully"));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  getAllUsers,
+  getUserById,
+  deleteUserById,
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,13 +3,39 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  getAllUsers,
+  getUserById,
+  deleteUserById
 } from "../controllers/user.controllers.js";
 import { verifyJWT, isAdmin } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
+// User Routes 
 
 router.route("/register").post(registerUser);
+
 router.route("/login").post(loginUser);
+
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+// Admin Routes
+
+router.route("/all-users").get( isAdmin, getAllUsers );
+router.route("/u/:id")
+  .get(isAdmin, getUserById)
+  .delete(isAdmin, deleteUserById)
+
 
 export default router;
